0c524808f759edc32b04b2114668e8fb4fe80807
mok <mok0230@protonmail.com>
2022-09-15 14:27:40 -0700

    make changes to example project

--- examples/defender-test-project/autotasks/hello-world/index.js
+++ examples/defender-test-project/autotasks/hello-world/index.js
@@ -1,5 +1,6 @@
 exports.handler = async function (event) {
   console.log(`Hello world from serverless`);
   console.log(JSON.stringify(event));
+  console.log('blah blah blah')
   return `Hello world from serverless`;
 };
